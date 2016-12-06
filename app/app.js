90847eea3acfbed49a585b969d47603ab7486ea4
zhangzju <zhangway19921221@163.com>
2016-12-06 19:30:29 +0800

    add lost files

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  * @authors Your Name (you@example.org)
  * @date    2016-08-05 15:40:46
  * @version $Id$
@@ -9,25 +9,4 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import Component from './component.jsx';
-=======
-import Button from './component.jsx';
->>>>>>> 0828128edbe720d4d9ee7f549c6e14f809f56e8d
-import './basic.css';
-
-main();
-
-function main() {
-<<<<<<< HEAD
-	const comments = [
-		{text: 'First comment!'},
-		{text: 'second comment'},
-		{text:  'third comment'}
-	];
-    comments.map(comment=>ReactDOM.render(<Component {...comment}/>, document.getElementById('app')));
-=======
-
-    ReactDOM.render(<Button />, document.getElementById('app'));
->>>>>>> 0828128edbe720d4d9ee7f549c6e14f809f56e8d
-}
--- app/component.jsx
+++ app/component.jsx
@@ -2,29 +2,7 @@
 
 import React from 'react';
 import './bootstrap.css';
-<<<<<<< HEAD
 import './basic.css';
-
-export default class Button extends React.Component {
-
-  constructor(props) {
-     super(props);
-   };
-
-
-  render(){
-    return (
-      <section className="section">
-        <h2 className="sub-title">Vertically Aligned Users</h2>
-        <div className="user">
-            <div className="user__avatar"></div>
-            <div className="user__description">
-                <h2 className="user__username">John Doe</h2>
-                <p className="user__excerpt">{this.props.text}</p>
-            </div>
-        </div>
-    </section>
-=======
 
 export default class Button extends React.Component {
 
@@ -33,7 +11,6 @@ export default class Button extends React.Component {
       <div className="container">
         <a className="btn btn-default">Button</a>
       </div>
->>>>>>> 0828128edbe720d4d9ee7f549c6e14f809f56e8d
     );
   }
 }
